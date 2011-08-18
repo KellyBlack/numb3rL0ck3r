f488fb08de69524af289ac5c3f14a3794131e259
Kelly Black <kjblack@gmail.com>
2011-08-18 15:50:19 -0400

    Initial check in for an authorization class.

---
+++ User/Authorize.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+#
+#
+# Copyright (c) 2011, Kelly Black (kjblack@gmail.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following
+#    disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided with
+#    the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#
+
+# This is the class used to decide if a client is authorized to access
+# the inforation requested.
+
+class Authorize :
+
+    def __init__(self,passPhrase="",hash="") :
+	self.setPassPhrase(passPhrase)
+	self.setRemoteHash(hash)
+
+
+    def setPassPhrase(self,value):
+	self.passPhrase = value
+
+    def setRemoteHash(self,hash) :
+	self.remoteHash = hash
+
+    def isAuthorized(self,message,hash) :
+	return(self.hmacSHA1(message,hash))
+
+    def hmacSHA1(self,message,hash) :
+	return(False)
+
