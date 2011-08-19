04d1739cdd1eb6635c64cdba1b24c95d9b0c3fdf
Kelly Black <kjblack@gmail.com>
2011-08-19 16:37:28 -0400

    Began the process of setting up a way to escape strings consistently

--- SQL/DataBase.py
+++ SQL/DataBase.py
@@ -37,19 +37,96 @@
 # This is the class used to decide if a client is authorized to access
 # the inforation requested.
 
+import pgdb
+
 class DataBase :
 
-    def __init__(self,password="",database="",host="") :
+    def __init__(self,password="",database="",host="",user="") :
 	self.setPassword(password)
-	self.setDatabase(database)
+	self.setDatabaseName(database)
 	self.setHost(host)
+	self.setUser(user)
 
 
     def setPassword(self,value):
 	self.password = value
 
-    def setDatabase(self,dbName) :
+    def getPassword(self):
+	return(self.password)
+
+
+    def setDatabaseName(self,dbName) :
 	self.databaseName = dbName
+
+    def getDatabaseName(self) :
+	return(self.databaseName)
+
 
     def setHost(self,host) :
 	self.hostName = host
+
+    def getHost(self) :
+	return(self.hostName)
+
+
+    def setUser(self,user):
+	self.userName = user
+
+    def getUser(self):
+	return(self.userName)
+
+    
+
+    def connect(self) :
+	self.db = pgdb.connect(user=self.getUser(),
+			       password=self.getPassword(),
+			       host=self.getHost(),
+			       database=self.getDatabaseName()) # dsn=None
+
+    def close(self) :
+	self.db.close()
+
+
+    def commit(self) :
+	self.db.commit()
+
+
+    def escapeDictionary(self,theDict) :
+	for key, value in theDict.iteritems():
+	    theDict[key] = pgdb.escape_string(value)
+
+
+    def escapeList(self,theList) :
+	lupe = 0
+	for entry in theList:
+	    theList[lupe] = pgdb.escape_string(entry)
+	    lupe += 1
+
+
+    def escapeItems(self,theItems) :
+	
+	if(type(theItems) == dict) :
+	    self.escapeDictionary(theItems)
+
+	elif(type(theItems)==list):
+	    self.escapeList(theItems)
+
+	elif(type(theItems)==str):
+	    return(pgdb.escape_string(theItems))
+	
+	else :
+	    return(pgdb.escape_string(str(theItems)))
+
+if (__name__ == "__main__") :
+
+    db = DataBase()
+    d = {"a":"b''","c":"'d"}
+    db.escapeItems(d)
+    print(d)
+
+    d = ["'a","b'","''c"]
+    db.escapeItems(d)
+    print(d)
+
+    print(db.escapeItems("heas''"))
+    print(db.escapeItems(1))
