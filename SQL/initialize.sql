93c8bed0ee9b0167aacf4f3b69b6f20e7edaac9b
Kelly Black <kjblack@gmail.com>
2011-08-31 14:00:25 -0400

    Started to add the code to initialize the data base tables.

--- SQL/initialize.sql
+++ SQL/initialize.sql
@@ -1,26 +1,27 @@
 
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
+DROP TABLE IF EXISTS Numb3rL0ck3r_user;
+CREATE TABLE Numb3rL0ck3r_user (
        userid bigint PRIMARY KEY DEFAULT nextval('user_userid_no_seq'),
        username text NOT NULL,
        password text,
        email text NOT NULL,
-       originalIPAddress text,
-       lastIPAddress text,
+       originalIPAddress text DEFAULT NULL,
+       lastIPAddress text DEFAULT NULL,
        creationDate timestamp DEFAULT CURRENT_TIMESTAMP,
+       lastPWChange timestamp DEFAULT CURRENT_TIMESTAMP,
        institutionid REFERENCES institution(institutionid) NOT NULL
 );
 
-DROP TABLE IF EXISTS administrativeRoles;
-CREATE TABLE administrativeRoles (
+DROP TABLE IF EXISTS Numb3rL0ck3r_administrativeRoles;
+CREATE TABLE Numb3rL0ck3r_administrativeRoles (
        administrativeRolesid bigint PRIMARY KEY DEFAULT nextval('administrativeRoles_administrativeRolesid_no_seq'),
        administrativeRoleTitle text,
        administrativeRolesDescription text
 );
 
-DROP TABLE IF EXISTS priviliges;
-CREATE TABLE priviliges (
+DROP TABLE IF EXISTS Numb3rL0ck3r_priviliges;
+CREATE TABLE Numb3rL0ck3r_priviliges (
        priviligesid bigint PRIMARY KEY DEFAULT nextval('priviliges_priviligesid_no_seq'),
        userid bigint REFERENCES user(userid),
        administrativeRolesid REFERENCES administrativeRoles(administrativeRolesid) ON DELETE CASCADE,
@@ -31,30 +32,30 @@ CREATE TABLE priviliges (
 );
 
 
-DROP TABLE IF EXISTS institution;
-CREATE TABLE institution (
+DROP TABLE IF EXISTS Numb3rL0ck3r_institution;
+CREATE TABLE Numb3rL0ck3r_institution (
        institutionid bigint PRIMARY KEY DEFAULT nextval('institution_institutionid_no_seq'),
        institutionName text,
        institutionDescription text
 );
 
-DROP TABLE IF EXISTS department;
-CREATE TABLE department (
+DROP TABLE IF EXISTS Numb3rL0ck3r_department;
+CREATE TABLE Numb3rL0ck3r_department (
        departmentid bigint PRIMARY KEY DEFAULT nextval('department_departmentid_no_seq'),
        departmentName text,
        departmentDescription text
 );
 
-DROP TABLE IF EXISTS laboratory;
-CREATE TABLE laboratory (
+DROP TABLE IF EXISTS Numb3rL0ck3r_laboratory;
+CREATE TABLE Numb3rL0ck3r_laboratory (
        laboratoryid bigint PRIMARY KEY DEFAULT nextval('laboratory_laboratoryid_no_seq'),
        laboratoryName text,
        laboratoryDescription text,
        laboratoryDate timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS laboratoryInstance;
-CREATE TABLE laboratoryInstance (
+DROP TABLE IF EXISTS Numb3rL0ck3r_laboratoryInstance;
+CREATE TABLE Numb3rL0ck3r_laboratoryInstance (
        laboratoryInstanceid bigint PRIMARY KEY DEFAULT nextval('laboratoryInstance_laboratoryInstanceid_no_seq'),
        laboratoryid bigint REFERENCES laboratory(laboratoryid) ON DELETE CASCADE,
        laboratoryInstanceName text,
